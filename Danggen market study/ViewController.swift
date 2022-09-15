f2577e370791c5a4e7441a7a3dad3eb7028aba7c
Ro YongHun <mogi3206@naver.com>
2022-09-15 22:17:39 +0900

    우측 상단 검색, 메뉴, 공지 네비게이션 컨트롤러 적용

--- Danggen market study/ViewController.swift
+++ Danggen market study/ViewController.swift
@@ -13,7 +13,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func moveByNotice(_ sender: Any) {
+        
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SearchVC") else {
+            return
+        }
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    
+    @IBAction func moveByMenu(_ sender: Any) {
+        
+        guard let uvc1 = self.storyboard?.instantiateViewController(withIdentifier: "MenuVC") else {
+            return
+        }
+        self.navigationController?.pushViewController(uvc1, animated: true)
+    }
+    
+    @IBAction func moveBySearch(_ sender: Any) {
+        guard let uvc2 = self.storyboard?.instantiateViewController(withIdentifier: "NoticeVC") else {
+            return
+        }
+        self.navigationController?.pushViewController(uvc2, animated: true)
+        
+    }
+    
+    
+    
+    
 }
 
