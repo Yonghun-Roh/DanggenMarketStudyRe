79f0157318fc2806588d1aef0c21077ddd18f9b0
Ro YongHun <mogi3206@naver.com>
2022-09-16 22:30:10 +0900

    로그인 뷰 완성.(오프라인) submit버튼 눌러서 로그인하면 홈 화면으로 이동까지 완료.

--- Danggen market study/Login.swift
+++ Danggen market study/Login.swift
@@ -6,9 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
     
+    
+    @IBAction func back(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e)
+            } else {
+                self.performSegue(withIdentifier: "ViewController", sender: self)
+            }
+        }
+    }
     
 }
-
+}
--- Danggen market study/Main.swift
+++ Danggen market study/Main.swift
@@ -9,5 +9,18 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBAction func moveByLogin(_ sender: Any) {
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") else {
+            return
+        }
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    @IBAction func moveByLoginVC(_ sender: Any) {
+        
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") else{
+            return
+        }
+        self.present(uvc, animated: true)
+    }
     
 }
