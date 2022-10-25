758fc29b2bec3c078f0f7152d7f8ea603b368109
Ro YongHun <mogi3206@naver.com>
2022-10-25 11:35:01 +0900

    로그아웃 및 세그웨이를 통해 로그인 창으로 이동

--- DanggenMarketStudy/MyDanggen.swift
+++ DanggenMarketStudy/MyDanggen.swift
@@ -6,7 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class MyDanggenViewController: UIViewController {
     
+    @IBAction func logOutPressed(_ sender: Any) {
+        
+        let firebaseAuth = Auth.auth()
+    do {
+      try firebaseAuth.signOut()
+//        dismiss(animated: true)
+//        self.presentingViewController?.dismiss(animated: true)
+    
+    } catch let signOutError as NSError {
+      print("Error signing out: %@", signOutError)
+    }
+      
+    }
 }
